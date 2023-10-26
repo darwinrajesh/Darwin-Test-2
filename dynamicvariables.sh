c02d40eb11f25d6ffbbc81a1c5cbdecf5d23df4b
Rajesh <drajesh@in.imshealth.com>
2023-10-26 22:41:05 +0530

    Variables

--- dynamicvariables.sh
+++ dynamicvariables.sh
@@ -4,3 +4,4 @@ TODAYDATE=$(date +%F)
 echo "Todays date is $TODAYDATE"
 
 echo -e "Welcome to Bash training,Today date is \e[32m ${TODAYDATE} \e[0m"
+echo -e "Number of action sessions are \e[32m $(who | wc -l) \e[0m"
