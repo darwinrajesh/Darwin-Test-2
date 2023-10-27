6cbaa38222f8d8c955eb032e89c86128ecf9d0c2
Rajesh <drajesh@in.imshealth.com>
2023-10-27 19:24:41 +0530

    variables.sh

--- Functions.sh
+++ Functions.sh
@@ -3,11 +3,12 @@ sample() {
 echo "I love jesus"
 }
 sample
+#echo -e "Todays date is \e[32m ${TODAYDATE} \e[0m"
 stat()
 {
 
     echo -e  "No. of sessions open are  \e[32m $(who | wc -l) \e[0m"
-    #TODAYDATE = $(date +%F)
-    echo -e "Todays date is \e[32m ${TODAYDATE} \e[0m"
+    echo -e \e[32m $(date +%F) \e[0m"
+    
 }
 stat
