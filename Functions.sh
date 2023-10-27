3c891b976f7121a9face1f6759549748d2bfa949
Rajesh <drajesh@in.imshealth.com>
2023-10-27 19:08:52 +0530

    variables.sh

--- Functions.sh
+++ Functions.sh
@@ -7,7 +7,7 @@ stat()
 {
 
     echo -e  "No. of sessions open are  \e[32m $(who | wc -l) \e[0m"
-    TODAYDATE = $(date +%F)
+    #TODAYDATE = $(date +%F)
     echo -e "Todays date is \e[32m ${TODAYDATE} \e[0m"
 }
 stat
