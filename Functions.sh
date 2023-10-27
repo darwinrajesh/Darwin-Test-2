fa9359bb89813d8c535e8e8c7f2cb5f99adb9112
Rajesh <drajesh@in.imshealth.com>
2023-10-27 19:34:51 +0530

    variables.sh

--- Functions.sh
+++ Functions.sh
@@ -9,6 +9,9 @@ stat()
 
     echo -e  "No. of sessions open are  \e[32m $(who | wc -l) \e[0m"
     echo -e "\e[32m $(date +%F) \e[0m"
+    echo "Load Average On The system is $(uptime | awk -F : '{print $NF}' | awk -F , '{print $1}')"
+    echo -e "\t stat function completed"
     
 }
+echo "Calling stat function"
 stat
